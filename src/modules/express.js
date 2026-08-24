4c219b31fa620714dbcb39604a96d3b473353a7c
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-08-24 07:46:07 -0300

    refactor: move modules into src/modules

--- src/modules/express.js
+++ src/modules/express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const UserModel = require('../src/models/user.model');
+const UserModel = require('../models/user.model');
 const app = express();
 
 app.use(express.json()); 
